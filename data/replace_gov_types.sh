fce8ff773089b61da4a6fb8e2589b865c5de44c9
vadim-ivlev <vadim.ivlev@gmail.com>
2015-03-05 04:48:07 +0300

    data

--- data/replace_gov_types.sh
+++ data/replace_gov_types.sh
@@ -1,42 +1,42 @@
-sed -i.bak 's/"General Purpose"/0/'
-sed -i.bak 's/"Drainage"/1/'
-sed -i.bak 's/"Water Supply"/2/'
-sed -i.bak 's/"Sewerage"/3/'
-sed -i.bak 's/"Air Transportation(Airports)"/4/'
-sed -i.bak 's/"Other and Unallocable"/5/'
-sed -i.bak 's/"Housing & Community Dev."/6/'
-sed -i.bak 's/"Soil & Water Conservation"/7/'
-sed -i.bak 's/"Solid Waste Management"/8/'
-sed -i.bak 's/"Industrial Development"/9/'
-sed -i.bak 's/"Other Health"/10/'
-sed -i.bak 's/"Other Multi-function Districts"/11/'
-sed -i.bak 's/"Hospitals"/12/'
-sed -i.bak 's/"Sewerage and Water Supply"/13/'
-sed -i.bak 's/"Gas Supply"/14/'
-sed -i.bak 's/"Fire Protection and Water Supply"/15/'
-sed -i.bak 's/"Welfare Institutions"/16/'
-sed -i.bak 's/"Parks & Recreation"/17/'
-sed -i.bak 's/"Electric Power"/18/'
-sed -i.bak 's/"Fire Protection"/19/'
-sed -i.bak 's/"Misc. Commercial Activities"/20/'
-sed -i.bak 's/"Sea and Inland Port Facilities"/21/'
-sed -i.bak 's/"Correctional Institutions"/22/'
-sed -i.bak 's/"Public Mass Transit Systems"/23/'
-sed -i.bak 's/"Other Natural Resources"/24/'
-sed -i.bak 's/"School District"/25/'
-sed -i.bak 's/"Dependent School System"/26/'
-sed -i.bak 's/"Irrigation"/27/'
-sed -i.bak 's/"Flood Control"/28/'
-sed -i.bak 's/"Natural Resources and Water Supply"/29/'
-sed -i.bak 's/"Highways - Nontoll"/30/'
-sed -i.bak 's/"Libraries"/31/'
-sed -i.bak 's/"Parking Facilities"/32/'
-sed -i.bak 's/"Education"/33/'
-sed -i.bak 's/"Mortgage Credit"/34/'
-sed -i.bak 's/"Cemeteries"/35/'
-sed -i.bak 's/"Reclamation"/36/'
-sed -i.bak 's/"Police Protection"/37/'
-sed -i.bak 's/"Toll Highways"/38/'
-sed -i.bak 's/"Other Public Welfare"/39/'
-sed -i.bak 's/"Other Corrections"/40/'
-
+echo "$1"
+sed -i.bak 's/"General Purpose"/0/' $1
+sed -i.bak 's/"Drainage"/1/' $1
+sed -i.bak 's/"Water Supply"/2/' $1
+sed -i.bak 's/"Sewerage"/3/' $1
+sed -i.bak 's/"Air Transportation(Airports)"/4/' $1
+sed -i.bak 's/"Other and Unallocable"/5/' $1
+sed -i.bak 's/"Housing & Community Dev."/6/' $1
+sed -i.bak 's/"Soil & Water Conservation"/7/' $1
+sed -i.bak 's/"Solid Waste Management"/8/' $1
+sed -i.bak 's/"Industrial Development"/9/' $1
+sed -i.bak 's/"Other Health"/10/' $1
+sed -i.bak 's/"Other Multi-function Districts"/11/' $1
+sed -i.bak 's/"Hospitals"/12/' $1
+sed -i.bak 's/"Sewerage and Water Supply"/13/' $1
+sed -i.bak 's/"Gas Supply"/14/' $1
+sed -i.bak 's/"Fire Protection and Water Supply"/15/' $1
+sed -i.bak 's/"Welfare Institutions"/16/' $1
+sed -i.bak 's/"Parks & Recreation"/17/' $1
+sed -i.bak 's/"Electric Power"/18/' $1
+sed -i.bak 's/"Fire Protection"/19/' $1
+sed -i.bak 's/"Misc. Commercial Activities"/20/' $1
+sed -i.bak 's/"Sea and Inland Port Facilities"/21/' $1
+sed -i.bak 's/"Correctional Institutions"/22/' $1
+sed -i.bak 's/"Public Mass Transit Systems"/23/' $1
+sed -i.bak 's/"Other Natural Resources"/24/' $1
+sed -i.bak 's/"School District"/25/' $1
+sed -i.bak 's/"Dependent School System"/26/' $1
+sed -i.bak 's/"Irrigation"/27/' $1
+sed -i.bak 's/"Flood Control"/28/' $1
+sed -i.bak 's/"Natural Resources and Water Supply"/29/' $1
+sed -i.bak 's/"Highways - Nontoll"/30/' $1
+sed -i.bak 's/"Libraries"/31/' $1
+sed -i.bak 's/"Parking Facilities"/32/' $1
+sed -i.bak 's/"Education"/33/' $1
+sed -i.bak 's/"Mortgage Credit"/34/' $1
+sed -i.bak 's/"Cemeteries"/35/' $1
+sed -i.bak 's/"Reclamation"/36/' $1
+sed -i.bak 's/"Police Protection"/37/' $1
+sed -i.bak 's/"Toll Highways"/38/' $1
+sed -i.bak 's/"Other Public Welfare"/39/' $1
+sed -i.bak 's/"Other Corrections"/40/' $1
